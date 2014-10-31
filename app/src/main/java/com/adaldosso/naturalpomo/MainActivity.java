adf88c245fbdaea078df2b3fd2967d5d0b848a8f
Alberto Dal Dosso <daldosso@gmail.com>
2014-10-31 17:43:25 +0100

    Added timer

--- app/src/main/java/com/adaldosso/naturalpomo/MainActivity.java
+++ app/src/main/java/com/adaldosso/naturalpomo/MainActivity.java
@@ -2,8 +2,10 @@ package com.adaldosso.naturalpomo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,21 +14,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView clock = (TextView) findViewById(R.id.clock);
+
+        new CountDownTimer(25 * 60 * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                int minutes = (int) ((millisUntilFinished / 1000) / 60);
+                clock.setText(String.format("%d:%02d", minutes, seconds % 60));
+            }
+
+            public void onFinish() {
+                clock.setText("done!");
+            }
+        }.start();
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
