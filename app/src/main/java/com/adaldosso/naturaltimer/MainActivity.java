cc23c6c1dab8591f693f4eb194fe6d17d6aa9b3b
Alberto Dal Dosso <daldosso@gmail.com>
2014-12-18 12:10:49 +0100

    Layout improvements

--- app/src/main/java/com/adaldosso/naturaltimer/MainActivity.java
+++ app/src/main/java/com/adaldosso/naturaltimer/MainActivity.java
@@ -1,8 +1,6 @@
 package com.adaldosso.naturaltimer;
 
 import android.app.Activity;
-import android.content.Context;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -11,16 +9,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+enum TimerType { WORK, REST }
 
 public class MainActivity extends Activity {
 
-    private AudioManager audioManager;
+    public static final int WORK_TIME = 25;
+    public static final int REST_TIME = 5;
     private CountDownTimer timer;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(getApplication(), R.raw.clock);
         startWork(null);
     }
 
@@ -40,34 +42,39 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void startTimer(int time) {
+    private void startTimer(int time, final TimerType timerType) {
         if (timer != null) {
             timer.cancel();
         }
         final TextView clock = (TextView) findViewById(R.id.clock);
-        audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
-        final MediaPlayer mp = MediaPlayer.create(getApplication(), R.raw.clock);
         timer = new CountDownTimer(time * 60 * 1000, 1000) {
 
             public void onTick(long millisUntilFinished) {
                 int seconds = (int) (millisUntilFinished / 1000);
                 int minutes = (int) ((millisUntilFinished / 1000) / 60);
-                mp.start();
+                if (timerType == TimerType.WORK) {
+                    mp.start();
+                }
                 clock.setText(String.format("%d:%02d", minutes, seconds % 60));
 //                audioManager.playSoundEffect(android.view.SoundEffectConstants.CLICK);
             }
 
             public void onFinish() {
                 clock.setText("done!");
+                if (timerType == TimerType.WORK) {
+                    startTimer(REST_TIME, TimerType.REST);
+                } else {
+                    startTimer(WORK_TIME, TimerType.WORK);
+                }
             }
         }.start();
     }
 
     public void startWork(View view) {
-        startTimer(25);
+        startTimer(WORK_TIME, TimerType.WORK);
     }
 
     public void startRest(View view) {
-        startTimer(5);
+        startTimer(REST_TIME, TimerType.REST);
     }
 }
